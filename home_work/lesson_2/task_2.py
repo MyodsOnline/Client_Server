bc8916740ce76c683126ed032e44a740a013ad03
Andrew Smith <diver.vlz@gmail.com>
2022-05-19 13:13:49 +0300

    lesson_2 task_2

--- home_work/lesson_2/task_2.py
+++ home_work/lesson_2/task_2.py
@@ -6,3 +6,27 @@
 orders.json. При записи данных указать величину отступа в 4 пробельных символа;
 Проверить работу программы через вызов функции write_order_to_json() с передачей в нее значений каждого параметра.
 """
+import json
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    file = 'data/orders.json'
+
+    with open(file, encoding='utf-8') as f:
+        orders = json.load(f)
+
+        order_data = {
+            'item': item,
+            'quantity': quantity,
+            'price': price,
+            'buyer': buyer,
+            'date': date,
+        }
+        orders['orders'].append(order_data)
+
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(orders, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    write_order_to_json('test_item', 'test_quantity', 'test_price', 'test_buyer', 'test_date')
