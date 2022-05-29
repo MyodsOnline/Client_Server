374ece62812de68dfef2ce02c91f3d7aac703136
Andrew Smith <diver.vlz@gmail.com>
2022-05-29 07:10:35 +0300

    server_logger created

---
+++ home_work/lesson_3/logging/server_logger.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'server.log')
+
+log = logging.getLogger('app.server')
+
+_formatter_file = logging.Formatter("%(asctime)s  %(levelname)-8s  %(module)-10s  %(message)s")
+_formatter_stream = logging.Formatter("%(levelname)-8s  %(message)s ")
+
+file_handler = TimedRotatingFileHandler(PATH, when='D', interval=1, encoding='utf-8')
+file_handler.setFormatter(_formatter_file)
+
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setFormatter(_formatter_stream)
+stream_handler.setLevel(logging.ERROR)
+
+log.addHandler(file_handler)
+log.addHandler(stream_handler)
+log.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    log.error('Error message')
