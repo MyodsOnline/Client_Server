cae22f1f252e4281ec99d2c155fe334defd47221
Andrew Smith <diver.vlz@gmail.com>
2022-05-30 12:32:41 +0300

    server_client created

--- home_work/lesson_3/logger/server_logger.py
+++ home_work/lesson_3/logger/server_logger.py
@@ -8,19 +8,22 @@ PATH = os.path.join(PATH, 'server.log')
 
 log = logging.getLogger('app.server')
 
-_formatter_file = logging.Formatter("%(asctime)s  %(levelname)-8s  %(module)-10s  %(message)s")
-_formatter_stream = logging.Formatter("%(levelname)-8s  %(message)s ")
+_formatter_file = logging.Formatter('%(asctime)s  %(levelname)-8s  %(module)-10s  %(message)s')
+_formatter_stream = logging.Formatter('%(asctime)s  %(levelname)-8s  %(message)s')
 
 file_handler = TimedRotatingFileHandler(PATH, when='D', interval=1, encoding='utf-8')
 file_handler.setFormatter(_formatter_file)
 
 stream_handler = logging.StreamHandler(sys.stderr)
 stream_handler.setFormatter(_formatter_stream)
-stream_handler.setLevel(logging.ERROR)
+stream_handler.setLevel(logging.WARNING)
 
 log.addHandler(file_handler)
 log.addHandler(stream_handler)
 log.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
-    log.error('Error message')
+    log.critical('Критическая ошибка')
+    log.error('Ошибка')
+    log.debug('Отладочное сообщение')
+    log.info('Информационное сообщение')
