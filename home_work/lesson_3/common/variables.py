f9fcb76a969b9af93f0acf302f55abe82b1d0d11
Andrew Smith <diver.vlz@gmail.com>
2022-06-08 17:54:55 +0300

    server.py update

--- home_work/lesson_3/common/variables.py
+++ home_work/lesson_3/common/variables.py
@@ -1,16 +1,23 @@
+import logging
+
 DEFAULT_PORT = 7777
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 ENCODING = 'utf-8'
+LOGGING_LEVEL = logging.DEBUG
+CONNECTION_TIMEOUT = 0.5
 
 ACTION = 'action'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
 
 USERNAME_DB = ['Guest', 'Гость']
 
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'message_text'
--- home_work/lesson_3/server.py
+++ home_work/lesson_3/server.py
@@ -1,11 +1,14 @@
-import socket
-import json
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 import sys
 import logging
+import argparse
+import select
+import time
 
 from common.utils import get_message, send_message
 from common.variables import DEFAULT_PORT, MAX_CONNECTIONS, ACTION, \
-    TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR, USERNAME_DB
+    TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR, USERNAME_DB, \
+    SENDER, MESSAGE, MESSAGE_TEXT, CONNECTION_TIMEOUT
 import logger.server_logger
 from decos import log
 
@@ -13,64 +16,111 @@ SERVER_LOGGER = logging.getLogger('app.server')
 
 
 @log
-def process_client_message(message):
+def process_client_message(message, messages_list, client):
     SERVER_LOGGER.debug(f'Message instance from client: {message}')
     if ACTION in message \
             and message[ACTION] == PRESENCE \
             and TIME in message \
             and USER in message \
             and message[USER][ACCOUNT_NAME] in USERNAME_DB:
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad request'
-    }
+        send_message(client, {RESPONSE: 200})
+        return
+    elif ACTION in message \
+            and message[ACTION] == MESSAGE \
+            and TIME in message \
+            and MESSAGE_TEXT in MESSAGE:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+    else:
+        send_message({
+            RESPONSE: 400,
+            ERROR: 'Bad request'
+        })
+        return
+
+
+@log
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Wrong port: {listen_port}. Only [1024:65535] available.')
+        sys.exit(1)
+
+    return listen_address, listen_port
 
 
 def main():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-            if listen_port <= 1024 or listen_port > 65535:
-                raise ValueError
-        else:
-            listen_port = DEFAULT_PORT
-    except IndexError:
-        SERVER_LOGGER.critical(f'Missing port number')
-        sys.exit(1)
-    except ValueError:
-        SERVER_LOGGER.critical(f'Wrong port number')
-        sys.exit(1)
+    listen_address, listen_port = arg_parser()
+    clients = []
+    messages = []
 
-    try:
-        if '-a' in sys.argv:
-            listen_address = int(sys.argv[sys.argv.index('-a') + 1])
-        else:
-            listen_address = ''
-    except ValueError:
-        SERVER_LOGGER.critical(f'missing IP address')
-        sys.exit(1)
+    # with socket(AF_INET, SOCK_STREAM) as sock:
+    #     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    #     sock.bind((listen_address, listen_port))
+    #     sock.listen(MAX_CONNECTIONS)
+    #     sock.settimeout(CONNECTION_TIMEOUT)
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    transport.bind((listen_address, listen_port))
-    transport.listen(MAX_CONNECTIONS)
-    print('waiting.........')
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind((listen_address, listen_port))
+    sock.settimeout(CONNECTION_TIMEOUT)
+    sock.listen(MAX_CONNECTIONS)
+
+    print(f'.....waiting connection from {listen_port}:{listen_address}')
+    SERVER_LOGGER.info(f'Server for {listen_port}:{listen_address} is running')
 
     while True:
-        client_socket, client_address = transport.accept()
-        SERVER_LOGGER.info(f'Connection with {client_address} established.')
         try:
-            message_client = get_message(client_socket)
-            SERVER_LOGGER.debug(f'Message "{message_client}" received.')
-            server_response = process_client_message(message_client)
-            SERVER_LOGGER.debug(f'Response to client: {server_response}')
-            send_message(client_socket, server_response)
-            SERVER_LOGGER.debug(f'Connection to client: {client_address} been closed.')
-            client_socket.close()
-        except (ValueError, json.JSONDecodeError):
-            SERVER_LOGGER.error('some kind of error in the client message')
-            client_socket.close()
+            client, client_address = sock.accept()
+        except OSError as err:
+            print('...incoming connections:', err.errno)
+            pass
+        else:
+            SERVER_LOGGER.info(f'.....connection with {client_address} established.....')
+            clients.append(client)
+
+        resv_data_list = []
+        send_data_list = []
+        err_list = []
+
+        if clients:
+            try:
+                resv_data_list, send_data_list, err_list = select.select(clients, clients, [], 0)
+            except OSError:
+                pass
+            except Exception as ex:
+                SERVER_LOGGER.debug(f'.....error: {ex} in select.....')
+
+        if resv_data_list:
+            for client_with_message in resv_data_list:
+                try:
+                    process_client_message(get_message(client_with_message), messages, client_with_message)
+                except:
+                    SERVER_LOGGER.info(f'.....client {client_with_message.getpeername()} disconnected.....')
+                    clients.remove(client_with_message)
+
+        if messages and send_data_list:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+
+            for waiting_client in send_data_list:
+                try:
+                    send_message(waiting_client, message)
+                except:
+                    SERVER_LOGGER.info(f'.....client {waiting_client.getpeername()} disconnected.....')
+                    waiting_client.close()
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
